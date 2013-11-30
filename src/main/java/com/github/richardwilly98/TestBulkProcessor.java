5f741a56c3d5ad70e112a89bc6c6a0e065a12126
Richard Louapre <richard.louapre@gmail.com>
2013-11-29 19:21:15 -0500

    Add ramp-up of concurrent requests

--- src/main/java/com/github/richardwilly98/TestBulkProcessor.java
+++ src/main/java/com/github/richardwilly98/TestBulkProcessor.java
@@ -1,15 +1,18 @@
 package com.github.richardwilly98;
 
+import static org.elasticsearch.client.Requests.clusterHealthRequest;
 import static org.elasticsearch.client.Requests.indexRequest;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.base.Stopwatch;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -22,27 +25,33 @@ public class TestBulkProcessor {
 
 	private static final ESLogger logger = Loggers
 			.getLogger(TestBulkProcessor.class);
-    public final static int DEFAULT_CONCURRENT_REQUESTS = 50;
-    public final static int DEFAULT_BULK_ACTIONS = 1000;
-    public final static TimeValue DEFAULT_FLUSH_INTERVAL = TimeValue.timeValueMillis(10);
-    public final static ByteSizeValue DEFAULT_BULK_SIZE = new ByteSizeValue(5, ByteSizeUnit.MB);
+	public final static int DEFAULT_CONCURRENT_REQUESTS = 50;
+	public final static int DEFAULT_BULK_ACTIONS = 1000;
+	public final static TimeValue DEFAULT_FLUSH_INTERVAL = TimeValue
+			.timeValueMillis(10);
+	public final static ByteSizeValue DEFAULT_BULK_SIZE = new ByteSizeValue(5,
+			ByteSizeUnit.MB);
 
-    private final BulkProcessor bulkProcessor;
+	// private final BulkProcessor bulkProcessor;
+	private BulkProcessor bulkProcessor;
 	private final TransportClient client;
 	private static final String INDEX_NAME = "dummy-"
 			+ System.currentTimeMillis();
 	private static final String TYPE_NAME = "my-type";
 	private static final long MAX = 1000000;
 
-	private long indexedDocumentCount = 0;
+	private AtomicInteger indexedDocumentCount = new AtomicInteger();
+
 	public TestBulkProcessor(TransportClient client) {
 		this.client = client;
-		this.bulkProcessor = BulkProcessor.builder(client, listener)
-				.setBulkActions(DEFAULT_BULK_ACTIONS)
-				.setConcurrentRequests(8)
-//				.setFlushInterval(DEFAULT_FLUSH_INTERVAL)
-				.setBulkSize(DEFAULT_BULK_SIZE)
-				.build();
+		logger.info("Available processor: {}", Runtime.getRuntime()
+				.availableProcessors());
+		// this.bulkProcessor = BulkProcessor.builder(client, listener)
+		// .setBulkActions(DEFAULT_BULK_ACTIONS)
+		// .setConcurrentRequests(Runtime.getRuntime().availableProcessors())
+		// // .setFlushInterval(DEFAULT_FLUSH_INTERVAL)
+		// .setBulkSize(DEFAULT_BULK_SIZE)
+		// .build();
 	}
 
 	public static void main(String[] args) {
@@ -50,17 +59,39 @@ public class TestBulkProcessor {
 		client.addTransportAddress(new InetSocketTransportAddress("localhost",
 				9300));
 		TestBulkProcessor test = new TestBulkProcessor(client);
-		test.testBulkProcessor();
+		int count = 0;
+		int processors = Runtime.getRuntime().availableProcessors() - 1;
+		while (count++ < 20) {
+			int count2 = 0;
+			while (count2++ < 3) {
+				test.testBulkProcessor(processors);
+			}
+			processors++;
+		}
 		client.close();
 	}
 
-	void testBulkProcessor() {
+	void testBulkProcessor(int processors) {
+		logger.info("Test bulk processor with concurrent request: {}", processors);
+		Stopwatch watcher = Stopwatch.createStarted();
 		try {
+			indexedDocumentCount.set(0);
+			bulkProcessor = BulkProcessor.builder(client, listener)
+					.setBulkActions(DEFAULT_BULK_ACTIONS)
+					.setConcurrentRequests(processors)
+					// .setFlushInterval(DEFAULT_FLUSH_INTERVAL)
+					.setBulkSize(DEFAULT_BULK_SIZE).build();
 			if (client.admin().indices().prepareExists(INDEX_NAME).get()
 					.isExists()) {
 				client.admin().indices().prepareDelete(INDEX_NAME).get();
 			}
 			client.admin().indices().prepareCreate(INDEX_NAME).get();
+			logger.info(
+					"Done Cluster Health, status: {}",
+					client.admin()
+							.cluster()
+							.health(clusterHealthRequest().waitForGreenStatus())
+							.get().getStatus());
 			for (int i = 0; i < MAX; i++) {
 				Map<String, Object> data = new HashMap<String, Object>();
 				data.put("name", "test-" + i);
@@ -69,17 +100,41 @@ public class TestBulkProcessor {
 						.source(XContentFactory.jsonBuilder().map(data)));
 			}
 			bulkProcessor.close();
-			Thread.sleep(10000);
-			logger.info("Number of documents indexed from afterBulk: {}", indexedDocumentCount);
+			logger.info(
+					"Done Cluster Health, status: {}",
+					client.admin()
+							.cluster()
+							.health(clusterHealthRequest().waitForGreenStatus())
+							.get().getStatus());
+			logger.info("Number of documents indexed from afterBulk: {}",
+					indexedDocumentCount.get());
 			client.admin().indices().prepareRefresh(INDEX_NAME).get();
 			long count = client.prepareCount(INDEX_NAME).get().getCount();
 			logger.info("Number of documents: {} in index {}", count,
 					INDEX_NAME);
 			if (count != MAX) {
-				throw new RuntimeException(String.format("Number of documents indexed %s does not match the target %s", count, MAX));
+				throw new RuntimeException(
+						String.format(
+								"Number of documents indexed %s does not match the target %s",
+								count, MAX));
 			}
 		} catch (Throwable t) {
 			logger.error("testBulkProcessor failed", t);
+		} finally {
+			watcher.stop();
+			logger.info("Elpased time: {}", watcher.toString());
+			if (client.admin().indices().prepareExists(INDEX_NAME).get()
+					.isExists()) {
+				client.admin().indices().prepareDelete(INDEX_NAME).get();
+				try {
+					client.admin()
+							.cluster()
+							.health(clusterHealthRequest().waitForGreenStatus())
+							.get().getStatus();
+				} catch (Throwable t) {
+					throw new RuntimeException(t);
+				}
+			}
 		}
 	}
 
@@ -90,13 +145,14 @@ public class TestBulkProcessor {
 
 		public void afterBulk(long executionId, BulkRequest request,
 				Throwable failure) {
-			throw new RuntimeException(failure);
 		}
 
 		public void afterBulk(long executionId, BulkRequest request,
 				BulkResponse response) {
-			indexedDocumentCount += response.getItems().length;
-
+			if (response.hasFailures()) {
+				throw new RuntimeException(response.buildFailureMessage());
+			}
+			indexedDocumentCount.addAndGet(response.getItems().length);
 		}
 	};
 }
